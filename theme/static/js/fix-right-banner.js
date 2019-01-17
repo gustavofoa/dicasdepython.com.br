35771b0a28f1d4f1f5a05f3f7a30323fe64e90bc
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2019-01-17 11:59:45 -0200

    fix banner position.

--- theme/static/js/fix-right-banner.js
+++ theme/static/js/fix-right-banner.js
@@ -2,19 +2,22 @@ var adsense_position = 0;
 
 window.onscroll = function scrollFunction() {
 
-    //last adsense banner
     var last_adsense_banner = document.getElementById("sidebar-banner");
+    var footer = document.querySelector('footer')
 
-    // console.log(window.pageYOffset, last_adsense_banner.offsetTop, adsense_position)
-
-    if (window.pageYOffset-100 > last_adsense_banner.offsetTop) {
-        if(adsense_position == 0)
+    if (window.pageYOffset-80 > last_adsense_banner.offsetTop) {
+        if(adsense_position == 0 
+            && !last_adsense_banner.classList.contains('fix-banner-close-to-footer"'))
             adsense_position = last_adsense_banner.offsetTop;
-        last_adsense_banner.classList.add("fix-bannner");
+        last_adsense_banner.classList.add("fix-banner");
     }
-    if (window.pageYOffset-100 < adsense_position) {
+    if (window.pageYOffset-80 < adsense_position || window.pageYOffset+600 > footer.offsetTop) {
         adsense_position = 0;
-        last_adsense_banner.classList.remove("fix-bannner");
+        last_adsense_banner.classList.remove("fix-banner");
     }
+    if(window.pageYOffset+600 > footer.offsetTop)
+        last_adsense_banner.classList.add("fix-banner-close-to-footer");
+    else
+        last_adsense_banner.classList.remove("fix-banner-close-to-footer");
 
 }
