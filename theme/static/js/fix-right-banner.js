320045b6d81e3b74a1f8c7dad417e24013238600
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2021-11-02 19:14:12 -0300

    fix add fix-banner class

--- theme/static/js/fix-right-banner.js
+++ theme/static/js/fix-right-banner.js
@@ -5,13 +5,13 @@ window.onscroll = function scrollFunction() {
     var last_adsense_banner = document.getElementById("sidebar-banner");
     var footer = document.querySelector('footer')
 
-    if (window.pageYOffset-80 > last_adsense_banner.offsetTop) {
+    if (window.pageYOffset-380 > last_adsense_banner.offsetTop) {
         if(adsense_position == 0 
             && last_adsense_banner.offsetTop > 100)
             adsense_position = last_adsense_banner.offsetTop;
         last_adsense_banner.classList.add("fix-banner");
     }
-    if (window.pageYOffset-80 < adsense_position) {
+    if (window.pageYOffset-380 < adsense_position) {
         adsense_position = 0;
         last_adsense_banner.classList.remove("fix-banner");
     }
